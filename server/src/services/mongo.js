ba907390d3cf669161e81955897e49fce8073c9e
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-06-13 15:13:11 +0500

    adding data from SpaceX api to our mongoDB

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-	'mongodb+srv://nasa-api:6q4o7ECRBMVSrSA4@nasacluster.crexwln.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
 	console.log('MongoDB connection ready!');
